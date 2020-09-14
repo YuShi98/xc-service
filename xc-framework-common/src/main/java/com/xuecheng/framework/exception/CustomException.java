d68431834bd13bc3e1a48524cd848b02c7c577df
Oranges1024 <18770851423@163.com>
2020-09-14 21:32:36 +0800

    修改异常类

--- xc-framework-common/src/main/java/com/xuecheng/framework/exception/CustomException.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/exception/CustomException.java
@@ -11,7 +11,7 @@ import com.xuecheng.framework.model.response.ResultCode;
  */
 public class CustomException extends RuntimeException {
 
-    private ResultCode resultCode;
+    private final ResultCode resultCode;
 
     public CustomException(ResultCode resultCode){
         super("错误代码:" + resultCode.code() + "错误信息：" + resultCode.message());
